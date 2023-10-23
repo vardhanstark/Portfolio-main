400a7e57ae964a42e974e0f86764365d0017473e
vardhan <vardhanchilukuri87@gmail.comCOM>
2023-10-23 23:37:17 +0530

    Portfolio

---
+++ src/components/About.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const About = () => {
+    return (
+        <div name="About" className='w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white'>
+            <div className='max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full'>
+                <div className='pb-8'>
+                    <p className='text-4xl font-bold inline border-b-4 border-gray-500'>About</p>
+                </div>
+                <p className='text-xl mt-20 max-sm:mt-10'>
+                Hello! C.NAGA VARDHAN, a third-year Computer Science and Engineering student Specializing in AI and ML at VIT - AP Amaravati. Passionate about technology, I'm determined to excel in the computer science field. Actively pursuing internships and work opportunities in dynamic settings to apply and enhance my skills. Eager to upskill in new technologies and constantly seeking to broaden my knowledge. Open to learning and growth.
+                </p>
+                <br />
+                
+            </div>
+        </div>
+    )
+}
+
+export default About
